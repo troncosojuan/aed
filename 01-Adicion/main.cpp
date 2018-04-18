0c1cd37b1973e9fe9397bc75e6d6e489d391b28c
Troncoso Juan <Troncosojuanbernabe@gmail.com>
2018-04-18 10:22:56 -0300

    Adicion
    Se realiza el programa

---
+++ 01-Adicion/main.cpp
@@ -0,0 +1,13 @@
+/* Obtener del usuario dos números y mostrarle la suma */
+#include <iostream>
+
+
+
+int main()
+{
+    int a,b;
+    std::cout << ("ingrese 2 numeros para la suma \n");
+    std::cin >> a >> b;
+    std::cout << a + b;
+
+}
