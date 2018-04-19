ccc09b1208bb4aa18d85f306e219a7716737c85c
Troncoso Juan <Troncosojuanbernabe@gmail.com>
2018-04-19 11:36:29 -0300

    Acomodan las partes
    En el comentario se agrega la parte sin usar assert

--- 02-EjemplosdeTiposdeDatos/main.cpp
+++ 02-EjemplosdeTiposdeDatos/main.cpp
@@ -3,24 +3,22 @@
 /*Diseñar un programa C++ que ejemplifique la aplicación de los tipos de datos
 vistos en clases.*/
 using namespace std;
-int a = 2.000;
-double b = 4252.257423;
-char c = 'a';
-bool d = false;
+
 
 int main()
 {
 
 
-   assert (true == 0 && "enteros 253  -  double 233.255  -  char a  -  bool false");
+   assert (true == 0 && "int 253  -  double 233.255  -  char a  -  bool false");
 
 
-    /*cout << ("a continuacion mostraremos los tipos de datos que puede tener una variable\n");*/
-    /*cout << ("\n\t") << a << ("\n\t") << b << ("\n\t") << c << ("\n\t") << d;
-
-    ("\n\t") << a << ("\n\t") << b << ("\n\t") << c << ("\n\t") << d
-
-    assert(("enteros 253  -  double 233.255  -  char a  -  bool false", a == b ));
+    /*
+    int a = 2.000;
+    double b = 4252.257423;
+    char c = 'a';
+    bool d = false;
+    cout << ("a continuacion mostraremos los tipos de datos que puede tener una variable\n");
+    cout << ("\n\t") << a << ("\n\t") << b << ("\n\t") << c << ("\n\t") << d;
     */
 
 
