6d0facac28f3a46d33372be32a31e32679c75275
Troncoso Juan <Troncosojuanbernabe@gmail.com>
2018-04-19 11:00:36 -0300

    Se realizan las partes faltantes de Adicion
    Se crea la representacion visual y textual y se crea el modelo IPO de ambos TPS

---
+++ 02-EjemplosdeTiposdeDatos/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <assert.h>
+/*Diseñar un programa C++ que ejemplifique la aplicación de los tipos de datos
+vistos en clases.*/
+using namespace std;
+int a = 2.000;
+double b = 4252.257423;
+char c = 'a';
+bool d = false;
+
+int main()
+{
+
+
+   assert (true == 0 && "enteros 253  -  double 233.255  -  char a  -  bool false");
+
+
+    /*cout << ("a continuacion mostraremos los tipos de datos que puede tener una variable\n");*/
+    /*cout << ("\n\t") << a << ("\n\t") << b << ("\n\t") << c << ("\n\t") << d;
+
+    ("\n\t") << a << ("\n\t") << b << ("\n\t") << c << ("\n\t") << d
+
+    assert(("enteros 253  -  double 233.255  -  char a  -  bool false", a == b ));
+    */
+
+
+}
+
